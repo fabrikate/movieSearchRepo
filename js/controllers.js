db9d0513a46e7458bedf54baa2143068fe8c0634
KateStearns <katestearns@mac.com>
2015-11-02 12:42:35 -0800

    add media type toggle

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,10 @@
 var app = angular.module("movieSearch");
 app.controller('searchController', function($scope, $http) {
+  $scope.types = ['movie', 'series', 'episode'];
+  $scope.media = {type: ''}
   $scope.findMovie = function() {
-    $http.get('http://www.omdbapi.com/?s='+ $scope.userSearch +'&y=&plot=short&r=json').then(function(data) {
-      console.log(data.data);
+    $http.get('http://www.omdbapi.com/?s='+ $scope.userSearch +'&y=&plot=short&type=' + $scope.media.type + '&r=json').then(function(data) {
+      console.log($scope.media.type);
       $scope.titles = data.data.Search;
     })
   }
@@ -10,12 +12,12 @@ app.controller('searchController', function($scope, $http) {
 
 app.controller('showController', function($scope, $http, $routeParams) {
   console.log($routeParams.titleId);
-  $http.get('http://www.omdbapi.com/?i=' + $routeParams.titleId + '&plot=short&r=json').then(function(data) {
+  $http.get('http://www.omdbapi.com/?i=' + $routeParams.titleId + '&plot=long&tomatoes=true&r=json').then(function(data) {
     console.log(data.data);
     $scope.movie = data.data;
   })
   $scope.getInfo = function() {
-    $http.get('http://www.omdbapi.com/?s='+ $scope.userSearch +'&y=&plot=short&r=json').then(function(data) {
+    $http.get('http://www.omdbapi.com/?s='+ $scope.userSearch +'&y=&plot=long&tomatoes=true&r=json').then(function(data) {
       console.log(data.data.Search);
       $scope.titles = data.data.Search;
     })
